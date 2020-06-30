3ae37a8f07734e26e8f6d78d2466f1b84ce348ba
maxjerdee <mjerdee@princeton.edu>
2020-06-30 19:08:44 -0400

    Fix Environment PORT

--- start.js
+++ start.js
@@ -1,5 +1,7 @@
 const app = require('./app');
 
-const server = app.listen(3000, () => {
+const PORT = process.env.PORT || 5000
+
+const server = app.listen(PORT, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
